a3a2aba900df896ba0eabaf75fccacf0c08a0d70
nk_mason <nk981888@gmail.com>
2018-05-22 23:53:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,29 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    template = '''{  extends "layout.html"  }
-{  block body  }
-    <div class="center-content error">
-        <h1>Oops! That page doesn't exist.</h1>
-        <h3>%s</h3>
-    </div>
-{  endblock  }
-''' % (request.url)
-    return render_template_string(template), 404
+from flask import Flask, request, render_template_string, render_template
+
+app = Flask(__name__)
+
+@app.route('/main')
+def hello_ssti():
+	person = {'name':"world", 'secret':"UGhldmJoZj8gYWl2ZnZoei5wYnovcG5lcnJlZg=="}
+	if request.args.get('name'):
+		person['name'] = request.args.get('name')
+	template = '''<h2>Hello %s!</h2>''' % person['name']
+	return render_template_string(template, person=person)
+
+####
+# Private function if the user has local files.
+###
+def get_user_file(f_name):
+	with open(f_name) as f:
+		return f.readlines()
+
+app.jinja_env.globals['get_user_file'] = get_user_file # Allows for use in Jinja2 templates
+
+if __name__ == "__main__":
+	app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
