efe2b68f5774207dc7f93a564e5dd58be12353dd
nk_mason <nk981888@gmail.com>
2018-05-23 22:52:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,14 @@
-'''
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
 from flask import Flask, request, render_template_string, render_template
 
 app = Flask(__name__)
 
-@app.route('/main')
+@app.route('/hello-template-injection')
 def hello_ssti():
-	person = {'name':"world", 'secret':"UGhldmJoZj8gYWl2ZnZoei5wYnovcG5lcnJlZg=="}
+	person = {'name':"null", 'secret':"nullctf{}"}
 	if request.args.get('name'):
 		person['name'] = request.args.get('name')
 	template = '''<h2>Hello %s!</h2>''' % person['name']
 	return render_template_string(template, person=person)
 
-####
-# Private function if the user has local files.
-###
-def get_user_file(f_name):
-	with open(f_name) as f:
-		return f.readlines()
-
-app.jinja_env.globals['get_user_file'] = get_user_file # Allows for use in Jinja2 templates
-
 if __name__ == "__main__":
 	app.run(debug=True)
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-'''
-from flask import Flask, request
-from jinja2 import Template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    name = request.args.get('name', 'guest')
-
-    t = Template("Hello " + name)
-    return t.render()
-
-if __name__ == "__main__":
-    app.run()
